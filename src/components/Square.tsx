994be8df4df8bc475d589a1128a61b1282c55880
Hallou-G-Y <hallou.ghiles@gmail.com>
2025-01-30 12:56:43 +0100

    initial commit

---
+++ src/components/Square.tsx
@@ -0,0 +1,15 @@
+// components/Square.js
+import React from 'react';
+import { Button } from '@mui/material';
+const Square = ({ value, onClick }:any) => {
+return (
+<Button
+variant="outlined"
+style={{ width: '100%', height: '100px' }}
+onClick={onClick}
+>
+{value}
+</Button>
+);
+};
+export default Square;
